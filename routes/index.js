6da2844ad9d12d33b2b8821f33c169cb1b073e03
tkrain23 <tk.rain09@gmail.com>
2019-01-24 21:43:50 -0800

    modifying route to detect json requests

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,12 @@ module.exports = (app) => {
     const page = req.query.page || 1
 
     Pet.paginate({}, {page: page}).then((results) => {
-        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+        if (req.header('content-type') == 'application/json') {
+            return res.json({ pets: pets, categories: categories });
+        } else {
+            return res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+        }
+
     });
   });
 }
